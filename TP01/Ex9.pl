8d13e93b103cc04cb9c5a8781d80448cae7d31da
Goncalo Batalhao Alves <up201806451@FEUPSIG.FE.UP.PT>
2020-09-30 12:26:31 +0100

    TP01

---
+++ TP01/Ex9.pl
@@ -0,0 +1,13 @@
+aluno(joao, paradigmas).
+aluno(maria, paradigmas).
+aluno(joel, lab2).
+aluno(joel, estruturas).
+frequenta(joao, feup).
+frequenta(maria, feup).
+frequenta(joel, ist).
+professor(carlos, paradigmas).
+professor(ana_paula, estruturas).
+professor(pedro, lab2).
+funcionario(pedro, ist).
+funcionario(ana_paula, feup).
+funcionario(carlos, feup). 
